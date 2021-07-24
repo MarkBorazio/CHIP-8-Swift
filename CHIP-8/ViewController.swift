a80da1b475f49f6d3e0aa8f4d36bd28dc94f0d41
Mark <markborazio@hotmail.com>
2021-07-24 23:00:42 +1000

    Initial Commit

---
+++ CHIP-8/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CHIP-8
+//
+//  Created by Mark Borazio [Personal] on 24/7/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
